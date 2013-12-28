1cad97a78b7dfeb14b84f9b151a87744e6318cd9
juanca202 <juancaaltamirano@yahoo.com>
2013-12-28 10:01:39 -0500

    barcode bugs

--- app/models/schemas/purchase.sql
+++ app/models/schemas/purchase.sql
@@ -1,7 +1,19 @@
 CREATE TABLE IF NOT EXISTS `purchase` (
 	`id` INTEGER PRIMARY KEY,
 	`lid` int(11) NOT NULL,
-	`cmid` int(11) NOT NULL,
-	`location` varchar(50) NOT NULL
-	`date` varchar(50) NOT NULL
+	`vid` int(11) NOT NULL,
+	`venue_platform` varchar(50) NOT NULL,
+	`lat` varchar(50) NOT NULL,
+	`lng` varchar(50) NOT NULL,
+	`currency` varchar(3) NOT NULL,
+	`timestamp` int(11) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `purchase_item` (
+  `id` INTEGER PRIMARY KEY,
+  `puid` INTEGER NOT NULL,
+  `pid` INTEGER NOT NULL,
+  `price` float(11,2) NULL,
+  `quantity` INTEGER NULL,
+  `unit` varchar(20) NULL
 );
